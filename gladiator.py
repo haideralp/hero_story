7dc03f1d5f70212c1f566e9cb3e320dc11083814
Syed Haider Abedi <haider.abedi@gmx.com>
2022-08-03 09:22:56 +0100

    Create gladiator.py

---
+++ gladiator.py
@@ -0,0 +1,20 @@
+# Story of Maximus
+
+
+# Defining my story1
+
+story1 = {
+    "start": "Maximus begins fighting to protect Rome and restore peace",
+    "middle": "during which he comes to realise he is far away from home for many years",
+    "end": "however to serve Rome is his mission so he will go above and beyond and save Rome",
+    "hero": "great saviour"  # add new key:value pair.
+}
+
+print(story1)  # printing the entire dictionary
+print(type(story1))  # printing type of story
+print(story1.keys())  # only keys printed here
+print(story1.values())  # only values print here
+
+print(story1["start"])  # print the start bit of story
+print(story1["middle"])  # print the middle bit of story
+print(story1["end"])   # print the end bit of story
